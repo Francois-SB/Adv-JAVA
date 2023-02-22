9fcbc6d15cc3f58fc195ec7565161dae4a3ec2e6
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-02-22 12:28:52 +0100

    horloge

---
+++ src/fr/ldnr/thread/ThreadTime.java
@@ -0,0 +1,43 @@
+package fr.ldnr.thread;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ThreadTime {
+
+  public static void main(String[] args) {
+    
+    Thread thread = new Thread(new MonRunnable(1000));
+
+ 
+
+    thread.start();
+  }
+
+  private static class MonRunnable implements Runnable {
+
+    private long delai;
+    DateFormat df = new SimpleDateFormat("HH:mm:ss");
+    
+    public MonRunnable(long delai) {
+      this.delai = delai;
+      //this.df = new SimpleDateFormat("HH:mm:ss");
+    }
+
+    @Override
+    public void run() {
+    	while(true) {
+	      try {
+	        Thread.sleep(delai);
+	        System.out.println(df.format(new Date()));
+	        for(int i=0; i<8;i++) {
+	        	System.out.println();
+	        }
+	      } catch (InterruptedException e) {
+	        e.printStackTrace();
+	      }
+    	}
+    }
+  }
+}
